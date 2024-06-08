4158bb1adc0cd4c8be0582a7b3bb61298f5c0a8e
Janvier-Irasubiza <irasubizajanvier99@gmail.com>
2024-06-08 18:16:02 +0200

    Additional models

--- conference_planning/models.py
+++ conference_planning/models.py
@@ -22,3 +22,38 @@ class Attendees(models.Model):
         
     def __str__(self) -> str:
         return self.names
+
+class Sponsor(models.Model):
+    organization = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    logo = models.ImageField(upload_to='images/')
+    summary = models.TextField(max_length=5000)
+    url = models.CharField(max_length=100) 
+    
+class Speaker(models.Model):
+    names = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    title = models.CharField(max_length=100)
+    bio = models.TextField(max_length=10000)
+    picture = models.ImageField(upload_to='images/')
+    
+class Event(models.Model):
+    event_title = models.CharField(max_length=300)
+    description = models.TextField(max_length=5000)
+    organizer = models.CharField(max_length=200)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+class Agenda(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    
+class Testimonial(models.Model):
+    names = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='images/')
+    testimony = models.TextField(max_length=1000)
--- conference_planning/urls.py
+++ conference_planning/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 registration_patterns = [
     path('local/', views.local_registration, name='local_registration'),
@@ -26,4 +28,4 @@ urlpatterns = [
     path('authenticate', views.auth, name="authenticate"),
     path('administration/', include((admin_urls, 'admin'), namespace='admin')),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
