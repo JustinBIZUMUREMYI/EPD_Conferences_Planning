b2bb58465c644bce063382b6af3ed9ec3546b0a1
JB Developer <justinbizumuremyi@Justins-MacBook-Pro.local>
2024-06-21 12:02:19 +0200

    changes in admin page

--- conference_planning/admin.py
+++ conference_planning/admin.py
@@ -1,16 +1,15 @@
 from django.contrib import admin
-from .models import Attendees, Testimonial, Panalist, Sponsorships, booth, Document, BookSponsorship, Sponsor,Countdown, Speaker
+from .models import Attendees, Testimonial, Panalist, Sponsorships, booth, Document, BookSponsorship, Sponsor,Countdown, Speaker,PreviousPhotos,PreviousVideos
 from django.contrib.auth.models import Group
 
 admin.site.register([Attendees])
 admin.site.register([Sponsorships])
-admin.site.register([Testimonial])
+admin.site.register([Testimonial, PreviousPhotos,PreviousVideos])
 admin.site.register([Sponsor])
 admin.site.register([Panalist])
 admin.site.register([booth])
 admin.site.register([BookSponsorship])
 admin.site.register([Document])
-
 admin.site.register([Countdown, Speaker])
 admin.site.unregister(Group)
 
