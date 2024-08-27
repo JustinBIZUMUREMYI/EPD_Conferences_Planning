4bd9b72ef3e3c2aecb9e1777d7b5e323e2776205
Justin BIZUMUREMYI <bizujustin1234@gmail.com>
2024-08-27 13:22:10 +0200

    changes on the agenda

--- Epd_Conference_Planning/settings.py
+++ Epd_Conference_Planning/settings.py
@@ -21,8 +21,8 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 
+LLOWED_HOSTS = ['www.epdconference.com', 'epdconference.com', 'localhost', '127.0.0.1']
 
-ALLOWED_HOSTS = ['www.epdconference.com', 'epdconference.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
