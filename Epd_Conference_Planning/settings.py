c2ab46440223cae9eeffcbe017c417d68a820d18
Justin BIZUMUREMYI <bizujustin1234@gmail.com>
2024-08-27 13:38:31 +0200

    chenges in the settings

--- Epd_Conference_Planning/settings.py
+++ Epd_Conference_Planning/settings.py
@@ -21,7 +21,7 @@ CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
 
 
-LLOWED_HOSTS = ['www.epdconference.com', 'epdconference.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['www.epdconference.com', 'epdconference.com', 'localhost', '127.0.0.1']
 
 
 
